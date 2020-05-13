5ffa1562cd93caf6648ab20167482dfc931e48c0
Arran Whiteford <arran.whiteford@vuw.ac.nz>
2020-05-13 18:57:33 +1200

    just tried a range of buffers

--- code/tide_gnss_crosses_resample.py
+++ code/tide_gnss_crosses_resample.py
@@ -53,73 +53,76 @@ rs.reset_index(drop=True,inplace=True)
 
 # =============================================================================
 # =============================================================================
-
-
-#find all the points with other points close by, add them to a list
-point_intersects_all = []
-index_all =[]
-
-to_skip = []
-
-for i in tqdm(range(0,rs.shape[0])):
-    
-    #if the inex has already been counted, then skip.
-    if i in to_skip:
-        continue
-    
-    # get points which are close in space, and far in time
-    intx = rs[ (rs.geometry.intersects(rs.iloc[i].geometry.buffer(20)) == True) #the 8 is 8m away
-              &  (  (rs.timestamp-rs.timestamp.iloc[i])>3600 )
-              ].index1.tolist()     
-    
-    # add these points to a list
-    if len(intx) > 0:
-        point_intersects_all.append(intx)
-        index_all.append(rs.index1.iloc[i])   
-    
-    to_skip += intx
-    
-    
-
+for buffer_size in [10,20,30,40,50,60]:
 # =============================================================================
 # =============================================================================
-
-#organise so that each cluster of points is represented by 1 row = 1 point, and the separate height chnges are added
-
-repeated = gdf.iloc[index_all] #points which are repeated
-repeated['intx_points'] = point_intersects_all
-
-
-time_ele_list_all = []
-for i,row in tqdm(repeated.iterrows()):
     
-    time_ele_list = [] # a list of tuples of time and elevations for each point
+    #find all the points with other points close by, add them to a list
+    point_intersects_all = []
+    index_all =[]
     
-    time_ele_list.append( [gdf.iloc[i].timestamp,
-                           gdf.iloc[i]['HGT(m)'],
-                           gdf.iloc[i]['HGT(m)']  - gdf.iloc[i]['HGT(m)'] ])
+    to_skip = []
     
-    for index in row.intx_points:
-        time_ele_list.append(  [gdf.iloc[index].timestamp,
-                                gdf.iloc[index]['HGT(m)'],
-                                gdf.iloc[index]['HGT(m)']  - gdf.iloc[i]['HGT(m)'] ])
-                                
+    for i in tqdm(range(0,rs.shape[0])):
+        
+        #if the inex has already been counted, then skip.
+        if i in to_skip:
+            continue
+        
+        # get points which are close in space, and far in time
+        intx = rs[ (rs.geometry.intersects(rs.iloc[i].geometry.buffer(buffer_size)) == True) #the 8 is 8m away
+                  &  (  (rs.timestamp-rs.timestamp.iloc[i])>3600 )
+                  ].index1.tolist()     
+        
+        # add these points to a list
+        if len(intx) > 0:
+            point_intersects_all.append(intx)
+            index_all.append(rs.index1.iloc[i])   
+        
+        to_skip += intx
+        
+        
     
-    time_ele_list_all.append(np.array(time_ele_list))
+    # =============================================================================
+    # =============================================================================
     
-                                
-repeated['time_ele'] = time_ele_list_all
-
-max_dh_f = lambda row : row[:,2].max()
-repeated['max_dh'] =  repeated.time_ele.apply(max_dh_f)
-min_dh_f = lambda row : row[:,2].min()
-repeated['min_dh'] =  repeated.time_ele.apply(min_dh_f)
-repeated['tide'] =  repeated.max_dh - repeated.min_dh
-
-
-repeated.drop(['time_ele','intx_points'],1).to_file('/Users/home/whitefar/DATA/FIELD_ANT_19/POST_FIELD/INTERSECTIONS/tides_10msample20mcluster.shp')
-repeated.to_pickle('/Users/home/whitefar/DATA/FIELD_ANT_19/POST_FIELD/INTERSECTIONS/tides_10msample20mcluster.pkl')
-  
+    #organise so that each cluster of points is represented by 1 row = 1 point, and the separate height chnges are added
+    
+    repeated = gdf.iloc[index_all] #points which are repeated
+    repeated['intx_points'] = point_intersects_all
+    
+    
+    time_ele_list_all = []
+    for i,row in tqdm(repeated.iterrows()):
+        
+        time_ele_list = [] # a list of tuples of time and elevations for each point
+        
+        time_ele_list.append( [gdf.iloc[i].timestamp,
+                               gdf.iloc[i]['HGT(m)'],
+                               gdf.iloc[i]['HGT(m)']  - gdf.iloc[i]['HGT(m)'] ])
+        
+        for index in row.intx_points:
+            time_ele_list.append(  [gdf.iloc[index].timestamp,
+                                    gdf.iloc[index]['HGT(m)'],
+                                    gdf.iloc[index]['HGT(m)']  - gdf.iloc[i]['HGT(m)'] ])
+                                    
+        
+        time_ele_list_all.append(np.array(time_ele_list))
+        
+                                    
+    repeated['time_ele'] = time_ele_list_all
+    
+    max_dh_f = lambda row : row[:,2].max()
+    repeated['max_dh'] =  repeated.time_ele.apply(max_dh_f)
+    min_dh_f = lambda row : row[:,2].min()
+    repeated['min_dh'] =  repeated.time_ele.apply(min_dh_f)
+    repeated['tide'] =  repeated.max_dh - repeated.min_dh
+    
+    
+    repeated.drop(['time_ele','intx_points'],1).to_file(f'/Users/home/whitefar/DATA/FIELD_ANT_19/POST_FIELD/INTERSECTIONS/tides_10msample_{buffer_size}mbuffer.shp')
+    repeated.to_pickle(f'/Users/home/whitefar/DATA/FIELD_ANT_19/POST_FIELD/INTERSECTIONS/tides_10msample_{buffer_size}mbuffer.pkl')
+    
+    print(f"done for buffer {buffer_size}m")
 # # =============================================================================
 # =============================================================================
       
