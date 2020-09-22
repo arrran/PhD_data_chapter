aeb009b4772586305fb65cbeee89e67952b2420b
Arran Whiteford <arranjcw@gmail.com>
2020-09-22 18:04:28 +1200

    added line 8 and polished plots of map and dhdt cross sections

--- code/REMOTE_SENSING/ICESAT/cross_channel.py
+++ code/REMOTE_SENSING/ICESAT/cross_channel.py
@@ -1,4 +1,4 @@
-x#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun 12 16:56:42 2020
@@ -71,6 +71,7 @@ class icesat_dataset:
                          'is3': LineString([(-381706.7103145397, -733633.1081130945),(-375189.2166156959, -726633.5887804579)]),
                          'is4': LineString([(-381896.7461368341, -733380.6368849602), ( -377024.528257302, -728195.0649845804)]),
                          'is5': LineString([(-378187.3930402951, -729042.2059732673), (-377786.1458335032, -728618.909909106)]),
+                         'is6': LineString([(-376438, -723116), (-378537,-733580 )]),
                          'is7': LineString([(-376691.5694304057, -722796.118348052), ( -378990.8284925717, -734052.5693451025)]),
                          'is8': LineString([(-376974.9258506759, -722791.6817553517), ( -379329.5927148979, -734035.883147971)]),
                          'is9': LineString([(-379830.9172270952, -726358.755056211), ( -377754.301185295, -724148.9706686384)]),
@@ -82,12 +83,15 @@ class icesat_dataset:
         self.is_lines['is2'] = scale(self.is_lines['is2'],15,15)
         self.is_lines['is3'] = scale(self.is_lines['is3'],1.5,1.5)
         self.is_lines['is5'] = scale(self.is_lines['is5'],20,20)
+        self.is_lines['is6'] = scale(self.is_lines['is6'],1.5,1.5)
         self.is_lines['is4'] = scale(self.is_lines['is4'],2,2)
         self.is_lines['is9'] = scale(self.is_lines['is9'],4,4)
         self.is_lines['is7'] = scale(self.is_lines['is7'],1.2,1.2)
         self.is_lines['is11'] = scale(self.is_lines['is11'],1.5,1.5)
         
         self.meta = pd.DataFrame({})
+        
+        self.channel_mid = gpd.read_file("/Volumes/arc_04/FIELD_DATA/K8621920/RES/SHARE_SHAPEFILES/interpolated_channel_mid.shp")
         
         print(f'For this dataset')
         print('cycle number, corresponding number of points')
@@ -109,7 +113,7 @@ class icesat_dataset:
         for key,value in zip(self.is_lines.keys(),self.is_lines.values()):
             plt.plot(value.xy[0],value.xy[1],label=key)
             plt.legend()
-        plt.plot(gd_chan.iloc[75:-300:10].geometry.x,gd_chan.iloc[75:-300:10].geometry.y,'g:',label='channel')
+        plt.plot(self.channel_mid.geometry.x,self.channel_mid.geometry.y,'g:',label='channel centre')
         plt.xlim([-382064.5722209641,-374781.1654740692])
         plt.ylim([-734075.0820404041, -722764.4514729496])
         plt.grid()
@@ -128,6 +132,7 @@ class icesat_dataset:
         
         Sets and Returns
         -------
+        self.gda_lines[line_name]
         gda_line: a geodataframe of icesat points over that icesat line
 
         """
@@ -168,7 +173,11 @@ class icesat_dataset:
         """
         does getdata_line() over all lines
         
+        i.e. gets all iceset data over all the icesat lines in study area 
+        
+        
         verb : if set to true, spits out info about how much data on each line
+        optional buff=2.5, buffer size
         """
         for line in list(self.is_lines.keys()):
             self.getdata_line(line[2:],verb=False,buff=buff)
@@ -237,7 +246,11 @@ class icesat_dataset:
                 print(f'There is no data for line {icesat_line_number}, cycle {cycle}')
                 return
             
-       
+        point_midchannel = np.array( LineString(gda_line.geometry.tolist()).intersection(LineString(self.channel_mid.geometry.tolist())) )
+        
+        if len(point_midchannel)>1:
+            point_midchannel = np.mean(point_midchannel,axis=0)
+
         
        # This dataframe has h_corr from cycle_number_from, and dhdt. Times are from cycle_number_from
         gda_line_diff = gpd.GeoDataFrame( gda_line[gda_line.cycle_number==cycle_number_from],geometry=gda_line.geometry ).reset_index(drop=True)
@@ -245,7 +258,7 @@ class icesat_dataset:
         gda_line_diff[f"h_corr_cycle_{cycle_number_till}"] = gda_line[gda_line.cycle_number==cycle_number_till].h_corr.reset_index(drop=True)
        
                     
-        gda_line_diff['dh'] =(gda_line[gda_line.cycle_number==cycle_number_from].h_corr.to_numpy() -
+        gda_line_diff['dh'] =- (gda_line[gda_line.cycle_number==cycle_number_from].h_corr.to_numpy() -
                               gda_line[gda_line.cycle_number==cycle_number_till].h_corr.to_numpy())
         
         # get the time in years between data points
@@ -260,6 +273,8 @@ class icesat_dataset:
         color = 'tab:blue'
         ax1.set_xlabel('x',fontsize=18)
         ax1.set_ylabel('elevation (m)', color=color,fontsize=18)
+        
+        
         
         ax1.plot(gda_line_diff.sort_values(by ='x' ).x,
                  gda_line_diff.sort_values(by ='x' )[f"h_corr_cycle_{cycle_number_from}"],'b-')
@@ -282,6 +297,8 @@ class icesat_dataset:
         color = 'tab:red'
         ax2.set_ylabel('rate of change in surface , (m/a)', color=color,fontsize=18)  # we already handled the x-label with ax1
         ax2.plot(gda_line_diff.x,gda_line_diff.dhdt,'ro')
+        ax2.vlines(point_midchannel[0],gda_line_diff.dhdt.min(),gda_line_diff.dhdt.max(),label='Channel midpoint')
+        
         ax2.tick_params(axis='y', labelcolor=color)
         ax2.grid()
         
@@ -304,13 +321,13 @@ class icesat_dataset:
 
         """
         
-        gd_chan = gpd.read_file("/Users/home/whitefar/DATA/FIELD_ANT_19/POST_FIELD/RES/PROCESSED_LINES_GISFILE/linedownchan.shp")
+        
 
         
        # This dataframe has h_corr from cycle_number_from, and dhdt. Times are from cycle_number_from
         dadh = gpd.GeoDataFrame( self.gda[self.gda.cycle_number==cycle_number_from],geometry=self.gda.geometry )
                     
-        dadh['dh'] =(self.gda[self.gda.cycle_number==cycle_number_from].h_corr.to_numpy() -
+        dadh['dh'] =-(self.gda[self.gda.cycle_number==cycle_number_from].h_corr.to_numpy() -
                               self.gda[self.gda.cycle_number==cycle_number_till].h_corr.to_numpy())
         
         # get the time in years between data points
@@ -322,12 +339,12 @@ class icesat_dataset:
         
         plt.figure(figsize=(15,15))
         plt.scatter(dadh.x,dadh.y,c=dadh.dhdt,cmap='Spectral_r',vmin=-0.8, vmax=1)
-        plt.plot(gd_chan.iloc[75:-300:10].geometry.x,gd_chan.iloc[75:-300:10].geometry.y,'g:')
+        plt.plot(self.channel_mid.geometry.x,self.channel_mid.geometry.y,'g:',label='channel centre')
         plt.legend(['surface channel low','melt_rate'])
         cb = plt.colorbar()
         cb.set_label('rate of elevation change, m/a')
         plt.grid()
-        plt.show()
+        # plt.show()
         
     def plot_dhdt_crosssection_map(self,cycle_number_from,cycle_number_till,icesat_line_number,buff=2.5):
         """
@@ -353,14 +370,13 @@ class icesat_dataset:
                 return
             
        
-        gd_chan = gpd.read_file("/Users/home/whitefar/DATA/FIELD_ANT_19/POST_FIELD/RES/PROCESSED_LINES_GISFILE/linedownchan.shp")
        # This dataframe has h_corr from cycle_number_from, and dhdt. Times are from cycle_number_from
         gda_line_diff = gpd.GeoDataFrame( gda_line[gda_line.cycle_number==cycle_number_from],geometry=gda_line.geometry ).reset_index(drop=True)
         gda_line_diff.rename(columns={"h_corr": f"h_corr_cycle_{cycle_number_from}"}, inplace=True)
         gda_line_diff[f"h_corr_cycle_{cycle_number_till}"] = gda_line[gda_line.cycle_number==cycle_number_till].h_corr.reset_index(drop=True)
        
                     
-        gda_line_diff['dh'] =(gda_line[gda_line.cycle_number==cycle_number_from].h_corr.to_numpy() -
+        gda_line_diff['dh'] =-(gda_line[gda_line.cycle_number==cycle_number_from].h_corr.to_numpy() -
                               gda_line[gda_line.cycle_number==cycle_number_till].h_corr.to_numpy())
         
         # get the time in years between data points
@@ -375,7 +391,7 @@ class icesat_dataset:
         plt.figure(figsize=(15,15))
         plt.scatter(gda_line_diff.x,gda_line_diff.y,c=gda_line_diff.dhdt,cmap='Spectral_r',vmin=-0.8, vmax=1,label ='melt_rate')
         plt.plot(polygon.exterior.xy[0],polygon.exterior.xy[1],label='is polygon')
-        plt.plot(gd_chan.iloc[75:-300:10].geometry.x,gd_chan.iloc[75:-300:10].geometry.y,'g:',label='surface channel low')
+        plt.plot(self.channel_mid.geometry.x,self.channel_mid.geometry.y,'g:',label='channel middle')
         plt.legend()
         cb = plt.colorbar()
         cb.set_label('rate of elevation change, m/a')
@@ -383,8 +399,175 @@ class icesat_dataset:
         plt.show()
     
 
+    def plot_multiple_dhdt_crosssection_map(self,cycle_number_from,cycle_number_till,icesat_lines,buff=300,
+                                            **kwargs):
+        """
+        
 
-# path = "/Volumes/arc_02/REMOTE_SENSING/ICESAT2/ds_subset_kamb_20200404.nc"
-# ds =  icesat_dataset(path)
-# ds.getdata_alllines()
-# ds.plot_dhdt_map(cycle_number_from=3,cycle_number_till=6)
+        Parameters
+        ----------
+        cycle_number_from : TYPE integer
+            DESCRIPTION. icesat cycle number
+        cycle_number_till : TYPE integer
+            DESCRIPTION. icesat cycle number
+        icesat_lines : TYPE list of lists of number and string 'up' or 'down'
+            DESCRIPTION e.g. icesat_lines = [[12,'up'],[11,'down'],[7,'up'],[3,'up']]
+
+        Returns
+        -------
+        None.
+        
+        Plot of the map of dhdt but with labelled lines
+
+        """
+        
+        dadh = gpd.GeoDataFrame( self.gda[self.gda.cycle_number==cycle_number_from],geometry=self.gda.geometry )
+        dadh['dh'] =- (self.gda[self.gda.cycle_number==cycle_number_from].h_corr.to_numpy() -
+                     self.gda[self.gda.cycle_number==cycle_number_till].h_corr.to_numpy())
+        # get the time in years between data points
+        dadh['dt'] = (self.gda[self.gda.cycle_number==cycle_number_till].utc_time.to_numpy()  - 
+                      self.gda[self.gda.cycle_number==cycle_number_from].utc_time.to_numpy()  ) 
+        dadh.dt = dadh.dt /  np.timedelta64(1, 'Y')
+        dadh['dhdt'] = dadh['dh'].to_numpy() / dadh['dt'].to_numpy()  
+        
+        dict_entrys = [[f'is{icesat_line_number[0]}',icesat_line_number[1]] for icesat_line_number in icesat_lines]
+        
+        polygons = [[self.is_lines[dict_entry[0]].buffer(buff),dict_entry[1]] for dict_entry in dict_entrys]
+
+        plt.figure(figsize=(15,15))
+        plt.scatter(dadh.x,dadh.y,c=dadh.dhdt,cmap='Spectral_r',**kwargs)
+        plt.plot(self.channel_mid.geometry.x,self.channel_mid.geometry.y,'g-')
+        i=65
+        for polygon, text_loc in polygons:   
+            
+            plt.plot(polygon.exterior.xy[0],polygon.exterior.xy[1],':',label='is polygon')
+            if text_loc == 'up':
+                plt.text(np.max(polygon.exterior.xy[0]),np.max(polygon.exterior.xy[1])+ buff/2,chr(i),size=30)
+            elif text_loc == 'down':
+                    plt.text(np.min(polygon.exterior.xy[0])- buff,np.min(polygon.exterior.xy[1])- buff,chr(i),size=30)
+            i += 1
+        plt.legend(['surface channel low','melt_rate'],fontsize=12)
+        cb = plt.colorbar()
+        cb.set_label('rate of elevation change, m/a',fontsize=12)
+        plt.grid()
+        plt.show()
+                
+        # icesat_line_numbers = [b[0] for b in icesat_lines]
+
+
+    def plot_multiple_dhdt_crosssection(self,cycle_number_from,cycle_number_till,icesat_lines):
+        """
+        MUST DO  plot_multiple_dhdt_map() first.
+
+        Parameters
+        ----------
+        cycle_number_from : TYPE integer
+            DESCRIPTION. icesat cycle number
+        cycle_number_till : TYPE integer
+            DESCRIPTION. icesat cycle number
+        icesat_lines : TYPE list of lists of number and string 'up' or 'down'
+            DESCRIPTION e.g. icesat_lines = [[12,'up'],[11,'down'],[7,'up'],[3,'up']]
+
+        Returns
+        -------
+        None.
+        
+        Plots of cross sections which correspond to the map made with  plot_multiple_dhdt_crosssection_map() 
+        """
+
+        # if data over that line hasnt been found using getdataline or all_lines, find it.
+        dict_entrys = [f'is{icesat_line_number[0]}' for icesat_line_number in icesat_lines]
+        icesat_line_numbers = [b[0] for b in icesat_lines]
+        
+        gda_line_diffs = []
+        point_midchannels = []
+        
+        # for each line do the cross section diff
+        for icesat_line_number in icesat_line_numbers:
+            # if data over that line hasnt been found using getdataline or all_lines, find it.
+            dict_entry = f'is{icesat_line_number}'
+            try:
+                gda_line = self.gda_lines[dict_entry]
+            except KeyError:
+                self.getdata_line(icesat_line_number)
+                gda_line = self.gda_lines[dict_entry]
+        
+            for cycle in [cycle_number_from,cycle_number_till]:
+                try:
+                    gda_line.dropna(axis='index',subset=['h_corr']).cycle_number.value_counts().loc[cycle]
+                except KeyError:
+                    print(f'There is no data for line {icesat_line_number}, cycle {cycle}')
+                    continue
+        
+            point_midchannel = np.array( LineString(gda_line.geometry.tolist()).intersection(LineString(self.channel_mid.geometry.tolist())) ) 
+        
+            point_midchannels.append( np.mean(point_midchannel,axis=0) )
+        
+        
+           # This dataframe has h_corr from cycle_number_from, and dhdt. Times are from cycle_number_from
+            gda_line_diff = gpd.GeoDataFrame( gda_line[gda_line.cycle_number==cycle_number_from],geometry=gda_line.geometry ).reset_index(drop=True)
+            gda_line_diff.rename(columns={"h_corr": f"h_corr_cycle_{cycle_number_from}"}, inplace=True)
+            gda_line_diff[f"h_corr_cycle_{cycle_number_till}"] = gda_line[gda_line.cycle_number==cycle_number_till].h_corr.reset_index(drop=True)
+        
+        
+            gda_line_diff['dh'] =- (gda_line[gda_line.cycle_number==cycle_number_from].h_corr.to_numpy() -
+                                  gda_line[gda_line.cycle_number==cycle_number_till].h_corr.to_numpy())
+        
+            # get the time in years between data points
+            gda_line_diff['dt'] = (gda_line[gda_line.cycle_number==cycle_number_till].utc_time.to_numpy()  - 
+                              gda_line[gda_line.cycle_number==cycle_number_from].utc_time.to_numpy()  ) 
+            gda_line_diff.dt = gda_line_diff.dt /  np.timedelta64(1, 'Y')
+        
+            gda_line_diff['dhdt'] = gda_line_diff['dh'].to_numpy()/gda_line_diff['dt'].to_numpy()
+            
+            gda_line_diffs.append(gda_line_diff)
+            del gda_line_diff
+            
+        n_plots = len(gda_line_diffs)
+        
+        fig, ax = plt.subplots(n_plots,1,figsize=(12,3*n_plots),dpi=500)
+
+        i= 65
+        for ax1,gda_line_diff,point_midchannel in zip(ax,gda_line_diffs,point_midchannels):
+        
+            color = 'tab:blue'
+        
+            ax1.set_ylabel(f'{chr(i)} \n elevation (m)', color=color,fontsize=10)
+            i += 1
+        
+            ax1.plot(gda_line_diff.sort_values(by ='x' ).x,
+                     gda_line_diff.sort_values(by ='x' )[f"h_corr_cycle_{cycle_number_from}"],'b-',
+                    label = f"{gda_line[gda_line.cycle_number==cycle_number_from].utc_time.mean().date()}")
+        
+            ax1.plot(gda_line_diff.sort_values(by ='x' ).x,
+                     gda_line_diff.sort_values(by ='x' )[f"h_corr_cycle_{cycle_number_till}"],'g-',
+                    label = f"{gda_line[gda_line.cycle_number==cycle_number_till].utc_time.mean().date()}")
+        
+            ax1.tick_params(axis='x', labelcolor=color)
+            
+            ax1.grid()
+        #     ax1.legend()
+        
+            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        
+            color = 'tab:red'
+             # we already handled the x-label with ax1
+            ax2.plot(gda_line_diff.x,gda_line_diff.dhdt,'ro', label='dh')
+            ax2.vlines(point_midchannel[0],gda_line_diff.dhdt.min(),gda_line_diff.dhdt.max(),label='Channel midpoint')
+        
+            ax2.tick_params(axis='y', labelcolor=color)
+            ax2.grid()
+            ax2.set_ylabel('surface change, (m/a)', color=color,fontsize=10)
+            
+        lines1, labels1 = ax1.get_legend_handles_labels()
+        lines2, labels2 = ax2.get_legend_handles_labels()
+        ax2.legend(lines1 + lines2, labels1 + labels2, loc=0)
+            
+            
+        ax1.set_xlabel('UTM, x',fontsize=18)
+        plt.show()
+
+                
+
+        
+        
--- code/REMOTE_SENSING/REMA/radarlines_over_REMA.py
+++ code/REMOTE_SENSING/REMA/radarlines_over_REMA.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+u#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 14 09:54:26 2020
